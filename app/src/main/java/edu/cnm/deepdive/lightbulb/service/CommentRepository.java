45ed2720b854c7e37999a817503a83e972cf20f1
Richmond <rbaafi@cnm.edu>
2020-03-28 19:17:00 -0600

    Created Service functionality.

---
+++ app/src/main/java/edu/cnm/deepdive/lightbulb/service/CommentRepository.java
@@ -0,0 +1,115 @@
+package edu.cnm.deepdive.lightbulb.service;
+
+import android.annotation.SuppressLint;
+import edu.cnm.deepdive.lightbulb.model.Comment;
+import edu.cnm.deepdive.lightbulb.model.Content;
+import edu.cnm.deepdive.lightbulb.model.Keyword;
+import io.reactivex.Completable;
+import io.reactivex.Single;
+import io.reactivex.schedulers.Schedulers;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class CommentRepository {
+
+  private static final int NETWORK_POOL_SIZE = 10;
+  private static final String OAUTH_HEADER_FORMAT = "Bearer %s";
+  private static final String ISO_DATE_FORMAT = "yyyy-MM-dd";
+
+  private final LightBulbService proxy;
+  private final Executor networkPool;
+  private final DateFormat formatter;
+
+  @SuppressLint("SimpleDateFormat")
+  private CommentRepository() {
+    proxy = LightBulbService.getInstance();
+    networkPool = Executors.newFixedThreadPool(NETWORK_POOL_SIZE);
+    formatter = new SimpleDateFormat(ISO_DATE_FORMAT);
+  }
+
+  public CommentRepository(LightBulbService proxy, Executor networkPool,
+      DateFormat formatter) {
+    this.proxy = proxy;
+    this.networkPool = networkPool;
+    this.formatter = formatter;
+  }
+
+  public static CommentRepository getInstance() {
+    return InstanceHolder.INSTANCE;
+  }
+
+  public Single<Comment> getCommentOfToday(String token, Date date) {
+    return getCommentOfToday(token, new Date());
+  }
+
+  public Single<Comment> getCommentOfDay(String token, Date date) {
+    return proxy.getCommentOfDay(String.format(OAUTH_HEADER_FORMAT, token), formatter.format(date))
+        .subscribeOn(Schedulers.from(networkPool));
+  }
+
+  public Single<List<Comment>> getAllQuotes(String token) {
+    return proxy.getAllComments(String.format(OAUTH_HEADER_FORMAT, token))
+        .subscribeOn(Schedulers.from(networkPool));
+  }
+
+  public Single<List<Keyword>> getAllKeywords(
+      String token, boolean includeNull, boolean includeEmpty) {
+    return proxy.getAllKeywords(String.format(OAUTH_HEADER_FORMAT, token), includeNull, includeEmpty)
+        .subscribeOn(Schedulers.from(networkPool));
+  }
+
+  public Completable save(String token, Comment comment) {
+    if (comment.getId() == null) {
+      return Completable.fromSingle(
+          proxy.postComment(String.format(OAUTH_HEADER_FORMAT, token), comment)
+              .subscribeOn(Schedulers.from(networkPool))
+      );
+    } else {
+      return Completable.fromSingle(
+          proxy.putComment(String.format(OAUTH_HEADER_FORMAT, token), comment, comment.getId())
+              .subscribeOn(Schedulers.from(networkPool))
+      );
+    }
+  }
+
+  public Completable remove(String token, Comment comment) {
+    if (comment.getId() != null) {
+      return proxy.deleteComment(String.format(OAUTH_HEADER_FORMAT, token), comment.getId())
+          .subscribeOn(Schedulers.from(networkPool));
+    } else {
+      return Completable.complete();
+    }
+  }
+
+  public Single<Comment> get(String token, UUID id) {
+    return proxy.getComment(String.format(OAUTH_HEADER_FORMAT, token), id)
+        .subscribeOn(Schedulers.from(networkPool));
+  }
+
+  public Single<List<Content>> getAllContent(String token) {
+    return getAllKeywords(token, true, false)
+        .subscribeOn(Schedulers.io())
+        .map((keywords) -> {
+          List<Content> combined = new ArrayList<>();
+          for (Keyword keyword : keywords) {
+            combined.add(keyword);
+            Collections.addAll(combined, keyword.getComments());
+          }
+          return combined;
+        });
+  }
+
+  private static class InstanceHolder {
+
+    private static final CommentRepository INSTANCE = new CommentRepository();
+
+  }
+
+}
---
+++ app/src/main/java/edu/cnm/deepdive/lightbulb/service/LightBulbService.java
@@ -0,0 +1,90 @@
+package edu.cnm.deepdive.lightbulb.service;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import edu.cnm.deepdive.lightbulb.BuildConfig;
+import edu.cnm.deepdive.lightbulb.model.Comment;
+import edu.cnm.deepdive.lightbulb.model.Keyword;
+import io.reactivex.Completable;
+import io.reactivex.Single;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import okhttp3.logging.HttpLoggingInterceptor.Level;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface LightBulbService {
+
+
+  @GET("comments")
+  Single<Comment> getRandomComment(@Header("Authorization") String oauthHeader);
+
+  @GET("comments/lightbulb")
+  Single<Comment> getCommentOfDay(
+      @Header("Authorization") String oauthHeader, @Query("date") String date);
+
+  @GET("comments")
+  Single<List<Comment>> getAllComments(@Header("Authorization") String oauthHeader);
+
+  @GET("comments/{id}")
+  Single<Comment> getComment(@Header("Authorization") String oauthHeader, @Path("id") UUID id);
+
+  @GET("keywords")
+  Single<List<Keyword>> getAllKeywords(
+      @Header("Authorization") String oauthHeader,
+      @Query("includeNull") boolean includeNull, @Query("includeEmpty") boolean includeEmpty);
+
+  @POST("comments")
+  Single<Comment> postComment(@Header("Authorization") String oauthHeader, @Body Comment comment);
+
+  @PUT("comments/{id}")
+  Single<Comment> putComment(
+      @Header("Authorization") String oauthHeader, @Body Comment comment, @Path("id") UUID id);
+
+  @DELETE("comments/{id}")
+  Completable deleteComment(
+      @Header("Authorization") String oauthHeader, @Path("id") UUID id);
+
+  static LightBulbService getInstance() {
+    return InstanceHolder.INSTANCE;
+  }
+
+  class InstanceHolder {
+
+    private static final String TIMESTAMP_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
+    private static final LightBulbService INSTANCE;
+
+    static {
+      Gson gson  = new GsonBuilder()
+          .setDateFormat(TIMESTAMP_FORMAT)
+          .excludeFieldsWithoutExposeAnnotation()
+          .create();
+      HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+      interceptor.setLevel(Level.BODY);
+      OkHttpClient client = new OkHttpClient.Builder()
+          .readTimeout(60, TimeUnit.SECONDS)
+          .addInterceptor(interceptor)
+          .build();
+      Retrofit retrofit = new Retrofit.Builder()
+          .addConverterFactory(GsonConverterFactory.create(gson))
+          .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+          .client(client)
+          .build();
+      INSTANCE = retrofit.create(LightBulbService.class);
+    }
+
+  }
+
+}
